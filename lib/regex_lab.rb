c1bdfe39b4405e0ed97a51706db2baad64c33a1f
Mark Stabler <mark@stablerwritingservices.com>
2020-02-28 05:24:49 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -12,7 +12,8 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  array = word.scan(/\b\w+/)
+  array.all?{|each_word| each_word.match(/\b[A-Z].+[.?!]/)}
 end
 
 def valid_phone_number?(phone)
